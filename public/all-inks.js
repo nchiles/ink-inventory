6e145449f856c5ed179b6607888575de48d4b965
Nate Chiles <nate.chiles@gmail.com>
2019-08-24 14:11:48 -0400

    add scroll to top and find ink in all-inks

--- public/all-inks.js
+++ public/all-inks.js
@@ -1,8 +1,34 @@
+//SCROLL TO TOP BUTTON
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollTopBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollTopBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  $("html, body").animate({ scrollTop: 0 })
+  // document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
+}
+
+//SEARCH-SCROLL TO INK
+$('#scrollform').on('submit', function(e){  
+  e.preventDefault(); 
+  var ink = '#' + $('#search').val();
+  window.scrollTo(0, $(ink).offset().top - $(window).height()/10);
+});
+
 //ADD INK DROPDOWN
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+/* When the user clicks on the button,toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
+  $("#inktoadd").focus();
 }
 
 // Close the dropdown menu if the user clicks outside of it
